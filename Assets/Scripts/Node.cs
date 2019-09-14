2166641a3c9fc3bebaf76e924f630b0e38543548
Leo-taro FUJIMOTO <lfujimot@e1r12p16.42.fr>
2019-09-14 18:39:32 +0200

    turrets

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+
+    public Color hoverColor;
+    public Vector3 positionOffset;
+    private GameObject turret;
+    private Renderer rend;
+    private Color startColor;
+    void Start() {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    void OnMouseDown() {
+        if (turret != null) {
+            Debug.Log("WE CANT BUILD HERE");
+            return;
+        }
+        
+        GameObject turretToBuild = BuildManager.instance.getTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+    void OnMouseEnter() {
+        rend.material.color = hoverColor; 
+    }
+
+    void OnMouseExit() {
+        rend.material.color = startColor;
+    }
+}
